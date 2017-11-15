c9f8f3688d1fc242a5a178f1bdaa8510930b3ee6
jomenoube60 <jo.menoube@gmail.com>
2017-11-15 11:30:26 +0100

    create html parser

---
+++ parser.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ */
+class HtmlParser
+{
+  private $html; # the document to parse
+
+  public function setInput($input)
+  {
+
+  }
+
+  public function getInput($input)
+  {
+    return $this->html;
+  }
+  public function parseElement($element)
+  {
+
+  }
+}
+
+
+ ?>
