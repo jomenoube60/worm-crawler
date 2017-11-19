348a250863a7003d0e66faccf94707c458fd1131
jomenoube60 <jo.menoube@gmail.com>
2017-11-19 15:36:17 +0100

    add comment

--- parser.php
+++ parser.php
@@ -10,17 +10,17 @@ class HtmlParser
   private $element;
 
 
-  public function setInput($input)
+  public function setInput($input) # set the content to parse. It should take an html document
   {
     $this->html = $input;
   }
 
-  public function getInput($input)
+  public function getInput($input) # render the raw content
   {
     return $this->html;
   }
 
-  public function setElement($open , $close)
+  public function setElement($open , $close) # set boundaries containing the researched string
   {
     $this->element = [$open , $close];
   }
@@ -29,13 +29,13 @@ class HtmlParser
   {
     return $this->element;
   }
-  public function parseElement()
+  public function parseElement() # parse the content to retrieve the researched element
   {
 
     $regex = "/".$this->element[0]."(.*)".$this->element[1]."/";
     echo $this->element[0]." ".$this->element[1];
     $output = preg_match_all($regex , $this->html , $d);
-
+    
     return $d;
   }
 }
