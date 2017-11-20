0402351ef1c308bffe5834d258212549c04d0077
jomenoube60 <jo.menoube@gmail.com>
2017-11-20 11:42:27 +0100

    temporary main modif

--- main.php
+++ main.php
@@ -7,32 +7,6 @@ include 'lib/worm.php';
  * Contain the main class , named Worm . Use of this web crawler should
  * only start from this class
  */
-class Worm
-{
 
-  public $curl;
-
-  public $parser;
-
-  // functions
-
-  public function __construct()
-  {
-  $this->curl = New Curler;
-
-  $this->parser = New HtmlParser;
-  }
-
-  public function fetchContent($url , $open , $close )
-  {
-    $content = $this->curl->getOutput() ;
-    $this->curl->setFile($url);
-    $this->parser->setInput($content);
-    $this->parser->setElement($open , $close);
-    $this->parser->parseElement();
-
-    return $this->parser->getElement();
-  }
-}
 
  ?>
